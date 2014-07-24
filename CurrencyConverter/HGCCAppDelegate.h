2e2c36ab28df70d6d177e866f8d24099ace21e49
Atish Narlawar <atish.narlawar@gmail.com>
2014-07-24 13:10:56 -0400

    Initial Commit

---
+++ CurrencyConverter/HGCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HGCCAppDelegate.h
+//  CurrencyConverter
+//
+//  Created by HUGE | Atish Narlawar on 7/24/14.
+//  Copyright (c) 2014 HugeInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
